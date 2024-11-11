0f6495c29f81122a569bef35271ad2f3a9e474d4
corra <corrado.marzagalli@gmail.com>
2024-11-11 17:29:35 +0100

    modified links

--- js/components/createNavbar.mjs
+++ js/components/createNavbar.mjs
@@ -1,12 +1,21 @@
 export default function createNavbar(pagesName){    
+    const root = '/sito-mkw80/';
     const navBar = document.createElement('nav');
     const ul = document.createElement('ul');
-    ul.id = 'pagesList';
+    ul.id = 'pagesList';        
 
     for(let i = 0; i < pagesName.length; i++){
         const li = document.createElement('li');
         const a = document.createElement('a');
-        a.href = pagesName[i].toLowerCase();
+        if(pagesName[i].toLowerCase === 'home'){
+            a.href = root;            
+        }
+        else if(root === window.location.pathname){
+            a.href = `./pages/${pagesName[i].toLowerCase()}`;
+        }
+        else{
+            a.href = pagesName[i].toLowerCase();
+        }
         a.textContent = pagesName[i].toUpperCase();
         li.appendChild(a);
         ul.appendChild(li);
